fdd17fbdf9fa072c1b147d5b80d3b54c6dd30363
Mike-an-santos <miguelansantos@gmail.com>
2026-06-12 22:03:50 +0100

    Delete preload.js

--- preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  onOpenPDF: (callback) => ipcRenderer.on('open-pdf', (event, payload) => callback(payload))
-});
